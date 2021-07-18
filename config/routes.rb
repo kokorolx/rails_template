b9aeae19005ebfc8434c9b00b8fdf96adff31e8d
TamL <kokoro.lehoang@gmail.com>
2021-07-18 12:13:27 +0700

    Add devise admin, update layout devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  namespace :admins do
+    get 'dashboard', to: 'dashboard#index'
+  end
+
   devise_for :users
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
