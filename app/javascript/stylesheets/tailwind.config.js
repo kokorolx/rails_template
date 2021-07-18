a77f6411fd5f17bc392ec004b1b48c62eb5ff479
TamL <kokoro.lehoang@gmail.com>
2021-07-18 11:45:56 +0700

    Update layout devise

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,3 +1,5 @@
+var plugin = require("tailwindcss/plugin");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -14,7 +14,7 @@ SimpleForm.setup do |config|
   # stack. The options given below are used to wrap the
   # whole input.
   config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors, valid_class: :field_without_errors do |b|
+    hint_class: :field_with_hint, error_class: :'field_with_errors', valid_class: :field_without_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
@@ -55,7 +55,7 @@ SimpleForm.setup do |config|
     # b.use :input, class: 'input', error_class: 'is-invalid', valid_class: 'is-valid'
     b.use :label_input
     b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
+    b.use :error, wrap_with: { tag: :div, class: :error }
 
     ## full_messages_for
     # If you want to display the full error message for the attribute, you can
@@ -85,7 +85,7 @@ SimpleForm.setup do |config|
   config.error_notification_tag = :div
 
   # CSS class to add for error notification helper.
-  config.error_notification_class = 'error_notification'
+  config.error_notification_class = 'error_notification text-red-500 mb-5'
 
   # Series of attempts to detect a default label method for collection.
   # config.collection_label_methods = [ :to_label, :name, :title, :to_s ]
